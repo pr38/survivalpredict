1f98d66b38cf11ce1f5c2db9b39ff5e5edb2b14a
Pouya Rezvanipour <pouyar3@yahoo.com>
2026-06-17 21:55:57 -0700

    up ver num

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath(os.path.pardir))
 project = 'survivalpredict'
 copyright = '2026, Pouya Rezvanipour'
 author = 'Pouya Rezvanipour'
-#release = '0.0.1'
+#release = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
